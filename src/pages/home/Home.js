e52104b169d16ad3a8f65dfcfa5b44210fa3c54d
Platform Host <platform.webhost@gmail.com>
2023-12-14 18:27:46 -0500

    reverted gradients back to 0.8

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -20,11 +20,11 @@ export default function Home() {
               rgba(
                 ${products[productInd].botGradient[0]},
                 ${products[productInd].botGradient[1]},
-                ${products[productInd].botGradient[2]}, 1.0) 0%,
+                ${products[productInd].botGradient[2]}, 0.8) 0%,
               rgba(
                 ${products[productInd].topGradient[0]},
                 ${products[productInd].topGradient[1]},
-                ${products[productInd].topGradient[2]}, 1.0) 100%)`
+                ${products[productInd].topGradient[2]}, 0.8) 100%)`
   } 
 
 
